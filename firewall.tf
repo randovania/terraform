47e447fabeb66265478c206e17777a795c5018b4
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-04-29 02:10:30 +0300

    Initial rules

---
+++ firewall.tf
@@ -0,0 +1,42 @@
+
+
+resource "digitalocean_firewall" "http_server" {
+  name = "http-server"
+
+  droplet_ids = [digitalocean_droplet.main.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "all"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "all"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.8.0"
+    }
+  }
+}
+
+# Set the variable value in *.tfvars file
+# or using -var="do_token=..." CLI option
+variable "do_token" {}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+# Create a web server
+resource "digitalocean_droplet" "main" {
+  name   = "ubuntu-s-1vcpu-1gb-fra1-01"
+  region = "fra1"
+  tags   = ["critical"]
+  size   = "s-1vcpu-2gb"
+  image  = "69463186"
+  monitoring = true
+}
+
+resource "digitalocean_floating_ip" "randovania_metroidprime_run" {
+  droplet_id = digitalocean_droplet.main.id
+  region = "fra1"
+}
