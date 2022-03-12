b048a887aab522496e72c326b727c99acbd58000
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-03-12 15:17:18 +0200

    Expose docker swarm port

--- firewall.tf
+++ firewall.tf
@@ -23,6 +23,12 @@ resource "digitalocean_firewall" "http_server" {
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "2377"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
   outbound_rule {
     protocol              = "tcp"
     port_range            = "all"
